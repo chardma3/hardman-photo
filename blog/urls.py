b0ae79e66ebd489b33cda9e4c7837d88fda85040
chardma3 <chardma3@hotmail.com>
2021-04-27 19:14:34 +0000

    Reset blog model

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.Postlist.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
-]
