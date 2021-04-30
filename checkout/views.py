afd2d9b222b0794b85dae1a1b6dd8d8f13c3a44e
chardma3 <chardma3@hotmail.com>
2021-04-30 17:26:36 +0000

    Added login required to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from .forms import OrderForm
@@ -32,6 +33,7 @@ def cache_checkout_data(request):
         return HttpResponse(content=e, status=400)
 
 
+@login_required
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
