472ddfbd9dae5bd1cfa4b2e517ec79b25e5edbb6
chardma3 <chardma3@hotmail.com>
2021-04-30 00:17:08 +0000

    fixed success msg on remove func in bag views

--- bag/views.py
+++ bag/views.py
@@ -89,7 +89,9 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(request,
+                             (f'Removed size {size.upper()} '
+                              f'{product.name} from your bag'))
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
