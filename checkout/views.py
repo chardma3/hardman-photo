6c9902d9265542ea173d5b1a3871b789d5c3e79b
chardma3 <chardma3@hotmail.com>
2021-07-21 13:46:17 +0000

    found bug: missing extra css in base.html which messed up checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -72,6 +72,7 @@ def checkout(request):
                         order_line_item.save()
                     else:
                         for size, quantity in item_data['items_by_size'].items():
+                            print("Size", size)
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
