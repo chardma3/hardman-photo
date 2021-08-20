a3146579ada783762f008ffd40c87c1227951ae5
chardma3 <chardma3@hotmail.com>
2021-08-20 18:39:27 +0000

    moved error templates to main app, fixed typos in templates and found stray code showing up in contact page

--- home/views.py
+++ home/views.py
@@ -6,6 +6,3 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-def page_not_found_view(request, exception):
-    return render(request, '404.html', status=404)
